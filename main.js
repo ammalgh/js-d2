df2028a63e92749a917ae51f25ec4bd0e2c14f7f
ammalgh <ammalgh22@gmail.com>
2024-05-22 17:36:13 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+
+for( let index = 24; index >= 0 ; index -=2){
+
+    console.log(index );
+
+}
+   
+
+
+// 2
+for (let i = 1; i<=10 ; i++){
+    let square = i*i;
+    console.log(square);
+
+}
+
+// 3
+for (let i=1; i<=20 ; i+=2){
+    console.log(i);
+}
+
+// 4
+
+// let a =0;
+//     b=1;
+//     c;
+//  for(let i=2; i<)
+
+
+    // 5
+    for (let i = 1; i <= 3; i++) {
+        // console.log(MultiplicationTablefor$i);
+        for (let j = 1; j <= 10; j++) {
+          let product = i * j;
+          console.log( ixj );
+        }
+        console.log();
+      }
