69a6bf3a8737e9c402635b02fcebfccbc18fb615
ammalgh <ammalgh22@gmail.com>
2024-05-22 18:37:11 +0300

    first commit

--- main.js
+++ main.js
@@ -37,11 +37,41 @@ function printFibonacciSequence(n) {
 
 
     // 5
-    for (let i = 1; i <= 3; i++) {
-        // console.log(MultiplicationTablefor$i);
-        for (let j = 1; j <= 10; j++) {
-          let product = i * j;
-          console.log( ixj );
-        }
-        console.log();
-      }
+    // for (let i = 1; i <= 3; i++) {
+    //     // console.log(MultiplicationTablefor$i);
+    //     for (let j = 1; j <= 10; j++) {
+    //       let product = i * j;
+    //       console.log( ixj );
+    //     }
+    //     console.log();
+    //   }
+
+
+    //   Lab 2
+    // sum
+    function getSum(a, b) {
+        return a + b;
+    }
+    x = getSum(3, 5);
+    console.log(x);
+
+    // sub
+    function sub(a, b) {
+        return a - b;
+    }
+    y= sub(8, 5);
+    console.log(y);
+
+    // mul
+    var mul =function(a,b){
+        return a*b;
+    }
+    z=mul(5,5);
+    console.log(z);
+
+    // division
+    function  division(a, b) {
+        return a / b;
+    }
+    y= division(10, 2);
+    console.log(y);
